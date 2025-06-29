ded0361918f54932c73ed00f2338c77475b72418
Fahmisi23 <fahm23021si@student.nurulfikri.ac.id>
2025-06-29 09:03:06 +0700

    praktikum 8

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\UnitKerjaController;
+use app\Http\Controllers\PegawaiController;
+use app\Http\Controllers\RuangController;
+use app\Http\Controllers\PeminjamanController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/unit-kerja', [UnitKerjaController::class, 'index']);
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::get('/ruang', [RuangController::class, 'index']);
+Route::get('/peminjaman', [PeminjamanController::class, 'index']);
